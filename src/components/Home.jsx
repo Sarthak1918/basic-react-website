fa0cde206d383a326cbd6838da33918e906ea90e
Sarthak1918 <pandasarthak12345@gmail.com>
2023-06-20 16:25:10 +0530

    Code Update
    Improved homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import '../styles/Home.scss'
+import vg from "../assets/2.webp"
 const Home = () => {
     return (
-        <div className='home'>
-            <main>
-                <h1>TechStar</h1>
-                <p>Solution to all your Problems</p>
-            </main>
-        </div>
+        <>
+            <div className='home'>
+                <main>
+                    <h1>TechStar</h1>
+                    <p>Solution to all your Problems</p>
+                </main>
+            </div>
+            <div className='home2'>
+                <img src={vg} alt='Graphics' />
+
+                <div>
+                    <p>We are your one and only Solution to tech problems you face everyday.</p>
+                </div>
+            </div>
+
+        </>
     )
 }
 
