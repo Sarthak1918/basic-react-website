a387b224deea8fbbdcef8d9ea399866f36b0088a
Sarthak1918 <pandasarthak12345@gmail.com>
2023-06-20 15:20:50 +0530

    Code update
    Navigation,Home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Home.scss'
+const Home = () => {
+    return (
+        <div className='home'>
+            <main>
+                <h1>TechStar</h1>
+                <p>Solution to all your Problems</p>
+            </main>
+        </div>
+    )
+}
+
+export default Home
