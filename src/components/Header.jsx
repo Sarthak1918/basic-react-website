2ba3639e3b44ede934c6f0be329ce041c31e6e03
Sarthak1918 <pandasarthak12345@gmail.com>
2023-06-26 20:15:32 +0530

    Code update
    Added login logout functionality using auth0.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
 import "../styles/Header.scss"
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 const Header = () => {
+    const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
     return (
         <nav>
             <HashLink to="/#home"><h1>TechStar</h1></HashLink>
@@ -13,6 +16,15 @@ const Header = () => {
                 <HashLink to={"/#about"}>About</HashLink>
                 <HashLink to={"/#brands"}>Brands</HashLink>
                 <Link to={"/services"}>Services</Link>
+                {
+                    isAuthenticated ?
+                        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                            Log Out
+                        </button> :
+                        <button onClick={() => loginWithRedirect()}>Log In</button>
+
+
+                }
             </main>
         </nav>
     )
--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-awtbqnl8suy6qj8c.us.auth0.com"
+    clientId="JOh8oMeNaQ289gqhsxNUDpcfwAPSjIeh"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
 );
 
